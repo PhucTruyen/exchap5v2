320025ff4043cbf6bd89d26da851861dccf3180a
Nguyen Phuc Truyen <aothuatao@gmail.com>
2024-09-15 19:01:31 +0700

    first commit

---
+++ src/main/java/com/mycompany/exchapter5_2/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package com.mycompany.exchapter5_2;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
